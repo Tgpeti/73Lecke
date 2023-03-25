0e620a0864727f9c10f619caae8d02eefed9a69f
Tgpeti <tgpeti@gmail.com>
2023-03-25 19:46:23 +0100

    init commit

---
+++ VisszaszamlaloFramework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Visszaszamlalas;
+
+namespace VisszaszamlaloFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime dt = new DateTime(2023, 04, 09);
+            Visszaszamlalo v = new Visszaszamlalo(dt);
+            v.MennyiMeg();
+            Console.WriteLine("csak nem megy");
+            Console.ReadLine();
+          
+
+
+        }
+    }
+}
---
+++ VisszaszamlaloNetCore/Program.cs
@@ -0,0 +1,17 @@
+﻿using Visszaszamlalas;
+
+namespace VisszaszamlaloNetCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2023, 04, 09);
+            Visszaszamlalo v = new Visszaszamlalo(dt);
+            v.MennyiMeg();
+            Console.WriteLine("ennyi meg egy bambi");
+            Console.ReadLine();
+            
+        }
+    }
+}
